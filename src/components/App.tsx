e80816cf3e76cf04d0312f330810aa19d81058fd
Angeredspy <christo.graham1@gmail.com>
2022-01-05 22:43:31 -0500

    Adds centered div styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,12 +3,22 @@ import { store } from '../state';
 import RepositoriesList from './RepositoriesList'; 
 
 const App = () => {
-  return <Provider store={store}> 
-    <div>
-      <h1>Search For a Package</h1>
-      <RepositoriesList/>
-    </div>
-  </Provider>
+  return (
+    <Provider store={store}>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <div>
+          <h1>Search For a Package!</h1>
+          <RepositoriesList />
+        </div>
+      </div>
+    </Provider>
+  );
 }; 
 
 export default App; 
--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -12,19 +12,27 @@ const RepositoriesList: React.FC = () => {
       searchRepositories(term);
     };
 
-    return ( 
-    <div>
-        <form onSubmit={onSubmit}>
-            <input value={term} onChange={e => setTerm(e.target.value)}/> 
+    return (
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <div>
+          <form onSubmit={onSubmit}>
+            <input value={term} onChange={(e) => setTerm(e.target.value)} />
             <button>Search</button>
-        </form>
-        {error && <h3>{error}</h3>}
-        {loading && <h3>Loading...</h3>}
-        {!error && !loading && 
-        data.map((name) => <div key={name}>{name}</div>)
-        }
-    </div>
-  ); 
+          </form>
+          {error && <h3>{error}</h3>}
+          {loading && <h3>Loading...</h3>}
+          {!error &&
+            !loading &&
+            data.map((name) => <div key={name}>{name}</div>)}
+        </div>
+      </div>
+    ); 
 }; 
 
 export default RepositoriesList;  
